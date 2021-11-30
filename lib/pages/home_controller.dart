bac20d7e184a06727214e44c558a729efeb778b6
Joscha von Andrian <joschavonandrian@gmail.com>
2021-11-30 01:23:40 +0100

    add css style overrides

--- lib/pages/home_controller.dart
+++ lib/pages/home_controller.dart
@@ -1,20 +1,43 @@
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 class HomeController extends GetxController {
+  InAppWebViewController? webViewController;
+
+  var tabIndex = 0;
+
+  void changeTabIndex(int index) {
+    tabIndex = index;
+    update();
+  }
+
   final box = GetStorage();
   RxBool get hideExplore => RxBool(box.read("hideExplore") ?? true);
   RxBool get hideStories => RxBool(box.read("hideStories") ?? false);
   RxBool get hideFeed => RxBool(box.read("hideFeed") ?? false);
 
-  void setHideExplore(bool val) => box.write("hideExplore", val);
-  void setHideStorie(bool val) => box.write("hideStories", val);
-  void setHideFeed(bool val) => box.write("hideFeed", val);
+  void setHide(String key, bool value) {
+    box.write(key, value);
+    webViewController?.reload();
+  }
 
   String cssHideFeed = """
-[role=main] {
+main > section > div:nth-child(3){
   display: none!important;
   }
+""";
+
+  String cssHideStories = """
+main > section > div:first-child {
+  display: none!important;
+}
+""";
+
+  String cssHideExplore = """
+[role=main] {
+   display: none!important;
+}
 """;
 
   String getHideCss(Uri? url) {
@@ -28,10 +51,18 @@ class HomeController extends GetxController {
           if (hideFeed.value) {
             css += cssHideFeed;
           }
+          if (hideStories.value) {
+            css += cssHideStories;
+          }
         }
 
         break;
       case "/explore":
+        {
+          if (hideExplore.value) {
+            css += cssHideExplore;
+          }
+        }
         break;
       default:
     }
